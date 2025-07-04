cd51b4d40b7ca6ce8bb79f6bd122319c69056e8a
Emre Erdem <emreerdem502@gmail.com>
2025-07-04 03:04:55 +0300

    cors policy edited

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(helmet());
 app.use(morgan("dev"));
